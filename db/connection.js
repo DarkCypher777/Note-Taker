2f3c6be02a52cefe673d58c3857b05b28e334899
Ben Diamond <54002015+DarkCypher777@users.noreply.github.com>
2019-10-31 16:44:25 -0400

    fix 3

--- db/connection.js
+++ db/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Password", // password here
+    password: "Pizzatime676", // password here
     database: "note_db"
   });
 }
